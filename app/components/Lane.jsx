dac4306a7db485c59a7452debeed2af7b3bf8892
Steve Good <steve@stevegood.org>
2015-08-30 13:32:04 -0500

    Working through book code

---
+++ app/components/Lane.jsx
@@ -0,0 +1,80 @@
+import AltContainer from 'alt/AltContainer';
+import React from 'react';
+import Editable from './Editable.jsx';
+import Notes from './Notes.jsx';
+
+import LaneActions from '../actions/LaneActions';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
+
+import { DropTarget } from 'react-dnd';
+import ItemTypes from './ItemTypes';
+
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const targetNote = targetProps.data || {};
+    const sourceProps = monitor.getItem();
+    const sourceNote = sourceProps.data || {};
+
+    if (!targetProps.notes.length) {
+      LaneActions.attachToLane({
+        laneId: targetProps.id,
+        noteId: sourceNote.id
+      });
+    }
+  }
+};
+
+@DropTarget(ItemTypes.NOTE, noteTarget, connect => ({
+  connectDropTarget: connect.dropTarget()
+}))
+export default class Lane extends React.Component {
+  render() {
+    const {connectDropTarget, id, name, notes, ...props} = this.props;
+
+    return connectDropTarget(
+      <div {...props}>
+        <div className="lane-header">
+          <Editable className="lane-name" value={name}
+                    onEdit={this.editName.bind(null, id)} />
+          <div className="lane-add-note">
+            <button onClick={this.addNote.bind(null, id)}>+</button>
+          </div>
+        </div>
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            items: () => NoteStore.get(notes)
+          }}>
+
+          <Notes
+            onEdit={this.editNote}
+            onDelete={this.deleteNote.bind(null, id)} />
+
+        </AltContainer>
+      </div>
+    );
+  }
+
+  addNote(laneId) {
+    NoteActions.create({task: 'New task'});
+    LaneActions.attachToLane({laneId});
+  }
+
+  editNote(noteId, task) {
+    NoteActions.update({id: noteId, task});
+  }
+
+  deleteNote(laneId, noteId) {
+    NoteActions.delete(noteId);
+    LaneActions.detachFromLane({laneId, noteId});
+  }
+
+  editName(id, name) {
+    if (name) {
+      LaneActions.update({id, name});
+    } else {
+      LaneActions.delete(id);
+    }
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
+var Clean = require('clean-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var pkg = require('./package.json');
+
+var TARGET = process.env.npm_lifecycle_event;
+var ROOT_PATH = path.resolve(__dirname);
+
+var common = {
+  entry: path.resolve(ROOT_PATH, 'app/main.jsx'),
+  output: {
+    path: path.resolve(ROOT_PATH, 'build'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
+};
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval',
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: path.resolve(ROOT_PATH, 'app')
+        },
+        {
+          test: /\.jsx?$/,
+          loaders: ['react-hot', 'babel'],
+          include: path.resolve(ROOT_PATH, 'app')
+        }
+      ]
+    },
+    devServer: {
+      colors: true,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {
+    entry: {
+      app: path.resolve(ROOT_PATH, 'app/main.jsx'),
+      vendor: Object.keys(pkg.dependencies)
+    },
+    output: {
+      path: path.resolve(ROOT_PATH, 'build'),
+      filename: 'app.[chunkhash].js'
+    },
+    devtool: 'source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: ExtractTextPlugin.extract('style', 'css'),
+          include: path.resolve(ROOT_PATH, 'app')
+        },
+        {
+          test: /\.jsx?$/,
+          loaders: ['babel'],
+          include: path.resolve(ROOT_PATH, 'app')
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('styles.css'),
+      new Clean(['build']),
+      new webpack.optimize.CommonsChunkPlugin(
+        'vendor',
+        'vendor.[chunkhash].js'
+      ),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  })
+}
