14e9b6c29584cb027d75b8e161b704b3ea810a98
Steve Good <steve.good@bnsf.com>
2015-09-02 11:05:15 -0500

    Wrapped editor with a Card.  It really ties the room together.

--- client/components/Editor.jsx
+++ client/components/Editor.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import {FontIcon, IconButton, TextField, Toolbar, ToolbarGroup, ToolbarSeparator} from 'material-ui';
+import {
+  Avatar,
+  Card,
+  CardHeader,
+  CardTitle,
+  CardText,
+  FontIcon,
+  TextField,
+  Toolbar,
+  ToolbarGroup,
+  ToolbarSeparator
+} from 'material-ui';
 
 class Editor extends React.Component {
   render() {
+    const {title} = this.props;
+    const avatar = <Avatar>{title[0].toUpperCase()}</Avatar>;
     return (
-      <div className='editor'>
-        <h2>{this.props.title}</h2>
-        <Toolbar>
-          <ToolbarGroup key={0}>
-            <FontIcon className='fa fa-bold' />
-            <FontIcon className='fa fa-italic' />
-            <FontIcon className='fa fa-underline' />
-            <ToolbarSeparator />
-          </ToolbarGroup>
-        </Toolbar>
+      <Card className='editor'>
+        <CardHeader
+          title={title}
+          avatar={avatar} />
+        <CardText>
+          <Toolbar>
+            <ToolbarGroup key={0}>
+              <FontIcon className='fa fa-bold' />
+              <FontIcon className='fa fa-italic' />
+              <FontIcon className='fa fa-underline' />
+              <ToolbarSeparator />
+            </ToolbarGroup>
+          </Toolbar>
 
-        <TextField
-          className="editor-field"
-          hintText="Notes..."
-          multiLine={true} />
-      </div>
+          <TextField
+            className="editor-field"
+            hintText="Notes..."
+            multiLine={true} />
+        </CardText>
+      </Card>
     );
   }
 }
